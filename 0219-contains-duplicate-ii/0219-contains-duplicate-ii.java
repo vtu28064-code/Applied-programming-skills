b405679631fb97810b22dc37c009bf5a36744f99
vtu28064-code <vtu28064@veltech.edu.in>
2026-08-04 10:05:05 +0530

    Time: 26 ms (45.4%), Space: 95.2 MB (24.68%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> window = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i > k) {
+                window.remove(nums[i - k - 1]);
+            }
+            if (!window.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
